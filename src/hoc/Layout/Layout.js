c1da5252680d33181d56597deccd6037229df724
zhibosheng <kage123zhibosheng@gmail.com>
2020-10-13 15:57:39 -0400

    add components

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Fragment } from "react";
+import PersistentDrawerLeft from "../../components/PersistentDrawerLeft";
+
+const layout = props => {
+    return (
+        <Fragment>
+            <PersistentDrawerLeft />
+        </Fragment>
+    );
+};
+
+export default layout;
